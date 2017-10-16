4619acaaed4420df3aea13aa9e4c5e24c1554e38
zabeen <zabeen.patel@ucl.ac.uk>
2017-10-16 14:28:05 +0100

    Added test for Quality of backstage pass increase by 2 when sellin <= 10

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -109,5 +109,22 @@ namespace csharp
 
 
         }
+
+        [Test]
+        public void Quality_IncreasesByTwo_WhenSellin10OrLess()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 9, Quality = 21},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 8, Quality = 22}
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+
+            Assert.AreEqual(22, items[0].Quality);
+            Assert.AreEqual(23, items[1].Quality);
+            Assert.AreEqual(24, items[2].Quality);
+        }
     }
 }
